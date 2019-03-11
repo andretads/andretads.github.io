655b696ad1774ace5aaa20c856ed666538e5b6f6
André Rabelo <andretads@gmail.com>
2019-03-10 23:16:54 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function (doc) {
+    const dropdownLink = doc.querySelector('#dropdownLink');
+    // const toggleButton = doc.querySelector('[data-toggle="popover"]');
+
+    dropdownLink.addEventListener('click', toggleDropDown, false);
+    // toggleButton.addEventListener('click', button, false);
+
+    function toggleDropDown() {
+        let navbarToggle = doc.querySelector('#navbar-toggle');
+
+        if (navbarToggle.className === 'topnav') {
+            navbarToggle.className += ' responsive';
+        } else {
+            navbarToggle.className = 'topnav';
+        }
+    }
+
+    // function button() {
+    //     toggleButton.popover();
+    // };
+
+})(document);
